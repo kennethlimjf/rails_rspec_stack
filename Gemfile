bdfa75892ce6e5c52edc0b467e1951e78dda2671
Kenneth Lim <kennethlimjf@gmail.com>
2014-07-20 21:10:07 +0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,38 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.1.1'
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.3'
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 2.0'
+gem 'haml-rails'
+gem 'bootstrap_form'
+
+group :development, :test do
+  gem 'rspec-rails', '~> 2.99'
+  gem 'fabrication'
+  gem 'faker'
+end
+
+group :development do
+  gem 'spring'
+end
+
+group :test do
+  gem 'shoulda-matchers', require: false
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'capybara-email'
+  gem 'selenium-webdriver'
+  gem 'launchy'
+  gem 'webmock'
+  gem 'vcr'
+end
+
+group :doc do
+  gem 'sdoc', '~> 0.4.0'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,48 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'shoulda/matchers'
+require 'capybara/rails'
+require 'capybara/email/rspec'
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.ignore_localhost = true
+end
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+  config.infer_spec_type_from_file_location!
+
+  # Database Cleaner Configuration, for use with Selenium tests
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
